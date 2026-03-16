62e75f2888e1f07f1b4bd38a8cfa537b2aff7bb8
jflinte <jflinte@clemson.edu>
2026-03-16 15:26:10 -0400

    created new branch

--- scripts/basic.py
+++ scripts/basic.py
@@ -1,21 +1,53 @@
 # imports
+import cv2
 import glob
 import os
+from typing import List
 import torch
 from depth_anything_3.api import DepthAnything3
 
+
+def jpg_to_png(images: List[str]) -> List[str]:
+    """
+    Converts list of jpg images to a list of png images
+    """
+    png_images: List[str] = []
+    for img_path in images:
+        if not img_path.lower().endswith('.jpg'):
+            continue
+        img = cv2.imread(img_path)
+        if img is None:
+            print(f"Failed to load {img_path}")
+            continue
+        png_path = img_path.rsplit('.', 1)[0] + '.png'
+        success = cv2.imwrite(png_path, img)
+        if success:
+            png_images.append(png_path)
+        else:
+            print(f"Failed to save {png_path}")
+    return png_images
+
 # constants
 MAX_IMAGES = 50
+IMAGE_TYPE = 'jpg'
 
 # set up model
 device = torch.device("cuda")
 model = DepthAnything3.from_pretrained("depth-anything/DA3NESTED-GIANT-LARGE")
 model = model.to(device=device)
 
+# Images
+example_path = "/home/df/data/jflinte/datasets/drone_dataset_brighton_beach/images"
 # example_path = "/app/data/datasets/kitti/data_odometry_color/sequences/01/image_2/"
-example_path = "./assets/examples/SOH"
-images = sorted(glob.glob(os.path.join(example_path, "*.png")))
-images = images[:50]  # take only first 50 images
+
+if IMAGE_TYPE == 'jpg':
+    images = sorted(glob.glob(os.path.join(example_path, "*.JPG")))
+    images = jpg_to_png(images)
+elif IMAGE_TYPE == 'png':
+    images = sorted(glob.glob(os.path.join(example_path, "*.png")))
+
+
+images = images[:MAX_IMAGES]  # take only first 50 images
 
 print(f"{len(images)} image/s found")
 
@@ -23,7 +55,7 @@ print(f"{len(images)} image/s found")
 # infer
 prediction = model.inference(
     images,  # type: ignore
-    export_dir='assets/test',
+    export_dir='/home/df/data/jflinte/Depth-Anything-3/assets/brighten_beach',
     export_format='glb-npz'
 
 )
